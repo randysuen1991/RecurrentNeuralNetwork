8ebf3056115010af0d2579ed1f8cc2cf4aaba97f
randysuen1991 <randysuen1991@gmail.com>
2018-04-03 15:19:39 +0800

    model.py updated

---
+++ RecurrentModel.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import RecurrentUnit as RU
+
+
+class Optimizer():
+    pass
+
+
+
+
+
+class ReccurentModel():
+    def __init__(self,input_dims,optimizer,dtype=tf.float64):
+        self.sess = tf.Session()
+        self.optimizer = optimizer
+        self.dtype = dtype
+        self.input_dims = input_dims
+        self.input_layer = tf.placeholder(dtype=self.dtype,shape=[None,None,input_dims])
+        # Presume the output to be the input
+        self.h = self.input
+        self.target = tf.placeholder(dtype=self.dtype,shape=[None,None,input_dims])
+        self.parameters = dict()
+        
+    # arguement 'recurrentunit' should be the object in RecurrentUnit.py
+    def Build(self,recurrentunit):
+        recurrentunit.input_layer = self.h
+        self.h = recurrentunit.h
+        
